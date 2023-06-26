a85b429c308600c2d1ed4a0f5a5f7d20aaace9ff
Cris Correa <artudvels@gmail.com>
2023-06-26 13:04:30 -0300

    Modificando el navbar responsive.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const reservasLink = document.getElementsByClassName('reservas')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
+    reservasLink.classList.toggle('active')
 })
